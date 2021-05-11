365e2757b1f723d42fa06f84fd2495a66ab4230d
Alessandro Conceição Santos <alessanbaixista@gmail.com>
2021-05-11 11:09:35 -0300

    Criando o repositorio

---
+++ Java/Exer1930.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Exer1930 {
+    public static void main(String[] args) {
+        Scanner ler = new Scanner(System.in);
+        int t1,t2,t3,t4;
+        int soma = 0;
+        t1 = ler.nextInt();
+        t2 = ler.nextInt();
+        t3 = ler.nextInt();
+        t4 = ler.nextInt();
+        System.out.println(soma = (t1+t2+t3+t4)-3);
+    }
+}
---
+++ Linguagem C/Exer_1924.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    char cursos[50];
+    scanf("%d", &n);
+
+    for (int i =0; i<n; i++){
+        scanf("%s", &cursos);
+    }
+    printf("Ciencia da Computacao\n");
+    return 0;
+}
